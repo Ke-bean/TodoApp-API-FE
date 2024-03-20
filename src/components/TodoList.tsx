3cbc7cde611d2cdfc793031483397eed363842ab
kebean <chenqiua@gmail.com>
2024-03-20 02:43:40 +0200

    finalised CRUD in components

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,6 +1,9 @@
 import React, {useState} from 'react'
 import TodoTypes from '../todo'
 import TodoService from '../TodoService'
+import { FaCheck, FaEdit } from 'react-icons/fa';
+import { GiCancel} from 'react-icons/gi';
+import { RiDeleteBin5Fill } from 'react-icons/ri';
 
 const TodoList = () => {
     const [todos, setTodos] = useState<TodoTypes[]>(TodoService.getTodos());
@@ -31,8 +34,32 @@ const TodoList = () => {
     setTodos(prev => prev.filter(todo => todo.id !== id));
     }
   return (
-    <div>
-
+    <div className='todoContainer'>
+      <div></div>
+      {todos.map(todo => (
+        <div className="items" key={todo.id}>
+          {editedTodoId == todo.id? (
+            <div className="editedText">
+              <input type="text" value={editedTodoText} onChange={e => setEditedTodoText(e.target.value)} autoFocus={true} />
+              <button onClick={() => handleEditTodoSave(todo.id)}>
+                <FaCheck/>
+              </button>
+              <button className='cancelBtn' onClick={() => handleEditCancel()}>
+                <GiCancel />
+              </button>
+            </div>
+          ):(
+            <div className="editBtn">
+              <button onClick={()=> handleEditStart(todo.id, todo.text)}>
+                <FaEdit />
+              </button>
+            </div>
+          )}
+          <button onClick={() => handleDeleteTodo(todo.id)}>
+            <RiDeleteBin5Fill />
+          </button>
+        </div>
+      ))}
     </div>
   )
 }
